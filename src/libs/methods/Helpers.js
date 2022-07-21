f1ef8acb51fdea87c30bef55cc7e0f1ae030b72f
Edoardo Lucini <assassineeeto@gmail.com>
2022-07-21 12:38:44 +0200

    added fetch asset function

---
+++ src/libs/methods/Helpers.js
@@ -0,0 +1,46 @@
+/* eslint-disable require-jsdoc */
+const axios = require("axios");
+const { Utils } = require("../utils/utils");
+
+class WaxBotHelpers {
+    static async fetchAsset(assetCollection, assetSchema) {
+        const URL = `https://wax.api.atomicassets.io/atomicassets/v1/assets?collection_name=${assetCollection}&schema_name=${assetSchema}&page=`;
+        let planets = [];
+        let lb;
+        const finish = false;
+
+        while (!finish) {
+            try {
+                const data = await axios.get(URL, {
+                    params: {
+                        lower_bound: lb,
+                        limit: 1000,
+                        order: "asc",
+                    },
+                });
+
+                if (data.data.data.length == 1) {
+                    break;
+                }
+
+                const limit = data.data.data.length - 1; // get the last planet index
+                lb = data.data.data[limit].asset_id; // use the last planed asset id as lower bound
+                planets = planets.concat(data.data.data); // add planets
+            } catch (err) {
+                console.log(err);
+                await Utils.sleep(2000);
+                return this.fetchAsset(assetCollection, assetSchema);
+            }
+        }
+        return planets;
+    }
+}
+
+module.exports = { WaxBotHelpers };
+
+// async function test() {
+//     const data = await Helpers.fetchAsset("darkgalaxies", "dark.planets");
+//     console.log(data[data.length - 1]);
+// }
+
+// test();
