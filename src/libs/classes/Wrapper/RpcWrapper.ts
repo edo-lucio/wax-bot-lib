86661fb1cf91a4cf5949b121c277dc54066c081e
Edoardo Lucini <assassineeeto@gmail.com>
2022-08-29 12:57:57 +0200

    moved

--- src/libs/classes/Wrapper/RpcWrapper.ts
+++ src/libs/classes/Wrapper/RpcWrapper.ts
@@ -9,6 +9,43 @@ class RpcWrapper {
         this.rpc = rpc;
     }
 
+    async getAssetBalance(
+        tokenDomain: string,
+        walletAddress: string,
+        tokenSymbol: string
+    ): Promise<any> {
+        try {
+            const tokenBalance = await this.rpc.get_currency_balance(
+                tokenDomain,
+                walletAddress,
+                tokenSymbol
+            );
+
+            if (tokenBalance.length) {
+                return parseFloat(tokenBalance[0].replace(/[A-Za-z]+/g, ""));
+            }
+            return 0.0;
+        } catch (error) {
+            console.log("\nCaught exception: " + error);
+            await Utils.sleep(8000);
+            return this.getAssetBalance(
+                tokenDomain,
+                walletAddress,
+                tokenSymbol
+            );
+        }
+    }
+
+    async getAccount(walletAddress: string): Promise<any> {
+        try {
+            const accountData = await this.rpc.get_account(walletAddress);
+            return accountData;
+        } catch (error) {
+            await Utils.sleep(4000);
+            return this.getAccount(walletAddress);
+        }
+    }
+
     async fetchTable({
         code,
         scope,
@@ -51,43 +88,6 @@ class RpcWrapper {
                 key_type,
                 reverse,
             });
-        }
-    }
-
-    async getAssetBalance(
-        tokenDomain: string,
-        walletAddress: string,
-        tokenSymbol: string
-    ): Promise<any> {
-        try {
-            const tokenBalance = await this.rpc.get_currency_balance(
-                tokenDomain,
-                walletAddress,
-                tokenSymbol
-            );
-
-            if (tokenBalance.length) {
-                return parseFloat(tokenBalance[0].replace(/[A-Za-z]+/g, ""));
-            }
-            return 0.0;
-        } catch (error) {
-            console.log("\nCaught exception: " + error);
-            await Utils.sleep(8000);
-            return this.getAssetBalance(
-                tokenDomain,
-                walletAddress,
-                tokenSymbol
-            );
-        }
-    }
-
-    async getAccount(walletAddress: string): Promise<any> {
-        try {
-            const accountData = await this.rpc.get_account(walletAddress);
-            return accountData;
-        } catch (error) {
-            await Utils.sleep(4000);
-            return this.getAccount(walletAddress);
         }
     }
 }
